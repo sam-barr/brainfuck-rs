b52fa4910efa9ebc943d4f8cf96f41e740da1086
Sam Barr <samfbarr@outlook.com>
2019-12-23 14:07:43 -0500

    Need to fix jumping

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-use std::io;
 use std::env;
 use std::fs::File;
+use std::io;
 use std::io::Read;
+use std::num::Wrapping;
 
 #[derive(Debug)]
 #[allow(non_camel_case_types)]
@@ -29,19 +30,31 @@ fn parse(code: &str) -> Vec<Token> {
             ',' => tokens.push(Token::T_COMMA),
             '[' => tokens.push(Token::T_LBRACKET),
             ']' => tokens.push(Token::T_RBRACKET),
-            _   => {}
+            _ => {}
         }
     }
 
     tokens
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 enum OpCode {
     Right,
     Left,
     Inc,
     Dec,
+    Print,
+    Read,
+    JumpIfZero(usize),
+    Jump(usize),
+}
+
+#[derive(Debug)]
+enum CondensedOpCode {
+    Right(u8),
+    Left(u8),
+    Inc(Wrapping<u8>),
+    Dec(Wrapping<u8>),
     Print,
     Read,
     JumpIfZero(usize),
@@ -55,25 +68,23 @@ fn compile(toks: &Vec<Token>) -> Result<Vec<OpCode>, &str> {
 
     for tok in toks {
         match tok {
-            Token::T_GT    => bytecode.push(OpCode::Right),
-            Token::T_LT    => bytecode.push(OpCode::Left),
-            Token::T_PLUS  => bytecode.push(OpCode::Inc),
+            Token::T_GT => bytecode.push(OpCode::Right),
+            Token::T_LT => bytecode.push(OpCode::Left),
+            Token::T_PLUS => bytecode.push(OpCode::Inc),
             Token::T_MINUS => bytecode.push(OpCode::Dec),
-            Token::T_DOT   => bytecode.push(OpCode::Print),
+            Token::T_DOT => bytecode.push(OpCode::Print),
             Token::T_COMMA => bytecode.push(OpCode::Read),
             Token::T_LBRACKET => {
                 brackets.push(idx);
                 bytecode.push(OpCode::JumpIfZero(0));
             }
-            Token::T_RBRACKET => {
-                match brackets.pop() {
-                    None => return Err("Unmatched right bracket!"),
-                    Some(j) => {
-                        bytecode.push(OpCode::Jump(j));
-                        bytecode[j] = OpCode::JumpIfZero(idx+1);
-                    }
+            Token::T_RBRACKET => match brackets.pop() {
+                None => return Err("Unmatched right bracket!"),
+                Some(j) => {
+                    bytecode.push(OpCode::Jump(j));
+                    bytecode[j] = OpCode::JumpIfZero(idx + 1);
                 }
-            }
+            },
         }
 
         idx += 1;
@@ -86,18 +97,70 @@ fn compile(toks: &Vec<Token>) -> Result<Vec<OpCode>, &str> {
     }
 }
 
+fn condense(opcodes: &Vec<OpCode>) -> Vec<CondensedOpCode> {
+    let mut grouped: Vec<(OpCode, u8)> = Vec::new();
+
+    for &opcode in opcodes {
+        if grouped.is_empty() {
+            grouped.push((opcode, 1));
+        } else {
+            let idx = grouped.len() - 1;
+            let (last_code, count) = grouped[idx];
+            if last_code == opcode {
+                grouped[idx] = (last_code, count + 1);
+            } else {
+                grouped.push((opcode, 1));
+            }
+        }
+    }
+
+    let mut condensed: Vec<CondensedOpCode> = Vec::new();
+
+    for (code, n) in grouped {
+        match code {
+            OpCode::Right => condensed.push(CondensedOpCode::Right(n)),
+            OpCode::Left => condensed.push(CondensedOpCode::Left(n)),
+            OpCode::Inc => condensed.push(CondensedOpCode::Inc(Wrapping(n))),
+            OpCode::Dec => condensed.push(CondensedOpCode::Dec(Wrapping(n))),
+            OpCode::Print => {
+                for _ in 0..n {
+                    condensed.push(CondensedOpCode::Print);
+                }
+            }
+            OpCode::Read => {
+                for _ in 0..n {
+                    condensed.push(CondensedOpCode::Read);
+                }
+            }
+            OpCode::JumpIfZero(j) => {
+                for _ in 0..n {
+                    condensed.push(CondensedOpCode::JumpIfZero(j));
+                }
+            }
+            OpCode::Jump(j) => {
+                for _ in 0..n {
+                    condensed.push(CondensedOpCode::Jump(j));
+                }
+            }
+        }
+    }
+
+    condensed
+}
+
 struct Tape {
-    left: Vec<u8>,
-    curr: u8,
-    right: Vec<u8>
+    left: Vec<Wrapping<u8>>,
+    curr: Wrapping<u8>,
+    right: Vec<Wrapping<u8>>,
 }
 
 impl Tape {
     fn new() -> Tape {
+        let zero = Wrapping(0);
         Tape {
-            left: vec![0,0,0,0,0,0,0,0],
-            curr: 0,
-            right: vec![0,0,0,0,0,0,0,0],
+            left: vec![zero, zero, zero, zero, zero, zero, zero, zero],
+            curr: zero,
+            right: vec![zero, zero, zero, zero, zero, zero, zero, zero],
         }
     }
 
@@ -105,7 +168,7 @@ impl Tape {
         self.right.push(self.curr);
         match self.left.pop() {
             Some(c) => self.curr = c,
-            None => self.curr = 0,
+            None => self.curr = Wrapping(0),
         }
     }
 
@@ -113,7 +176,20 @@ impl Tape {
         self.left.push(self.curr);
         match self.right.pop() {
             Some(c) => self.curr = c,
-            None => self.curr = 0,
+            None => self.curr = Wrapping(0),
+        }
+    }
+
+    // TODO: optimize the hell out of this
+    fn move_leftn(&mut self, n: u8) {
+        for _ in 0..n {
+            self.move_left();
+        }
+    }
+
+    fn move_rightn(&mut self, n: u8) {
+        for _ in 0..n {
+            self.move_right();
         }
     }
 }
@@ -130,47 +206,37 @@ fn fill_buffer(buffer: &mut Vec<u8>) {
     }
 }
 
-fn inc(x: &mut u8) {
-    if *x == 255 {
-        *x = 0;
-    }
-    else {
-        *x += 1;
-    }
-}
-
-fn dec(x: &mut u8) {
-    if *x == 0 {
-        *x = 255;
-    }
-    else {
-        *x -= 1;
-    }
-}
-
-fn interpret(bytecode: &Vec<OpCode>) {
+fn interpret(bytecode: &Vec<CondensedOpCode>) {
     let mut tape = Tape::new();
     let mut idx: usize = 0;
 
-    let mut buffer = vec![];
+    let mut buffer: Vec<u8> = Vec::new();
 
     while idx < bytecode.len() {
-        //println!("{}: {}, {:?}", idx, tape.curr, bytecode[idx]);
         match bytecode[idx] {
-            OpCode::Right => tape.move_right(),
-            OpCode::Left  => tape.move_left(),
-            OpCode::Inc   => inc(&mut tape.curr),
-            OpCode::Dec   => dec(&mut tape.curr),
-            OpCode::Print => print!("{}", tape.curr as char),
-            OpCode::Read => match buffer.pop() {
-                Some(c) => tape.curr = c,
-                None => { fill_buffer(&mut buffer); tape.curr = buffer.pop().unwrap(); }
-            }
-            OpCode::JumpIfZero(j) => match tape.curr {
-                0 => { idx = j; continue; }
+            CondensedOpCode::Right(n) => tape.move_rightn(n),
+            CondensedOpCode::Left(n) => tape.move_leftn(n),
+            CondensedOpCode::Inc(n) => tape.curr += n,
+            CondensedOpCode::Dec(n) => tape.curr -= n,
+            CondensedOpCode::Print => print!("{}", tape.curr.0 as char),
+            CondensedOpCode::Read => match buffer.pop() {
+                Some(c) => tape.curr = Wrapping(c),
+                None => {
+                    fill_buffer(&mut buffer);
+                    tape.curr = Wrapping(buffer.pop().unwrap());
+                }
+            },
+            CondensedOpCode::JumpIfZero(j) => match tape.curr.0 {
+                0 => {
+                    idx = j;
+                    continue;
+                }
                 _ => {}
+            },
+            CondensedOpCode::Jump(j) => {
+                idx = j;
+                continue;
             }
-            OpCode::Jump(j) => { idx = j; continue; }
         }
 
         idx += 1;
@@ -186,11 +252,14 @@ fn main() {
         file.read_to_string(&mut code).unwrap();
         let toks = parse(&code);
         match compile(&toks) {
-            Ok(bytecode) => interpret(&bytecode),
+            Ok(bytecode) => {
+                let bytecode = condense(&bytecode);
+                println!("{:?}", bytecode);
+                interpret(&bytecode);
+            }
             Err(e) => println!("{}", e),
         }
-    }
-    else {
+    } else {
         println!("please provide a file name");
     }
 }
